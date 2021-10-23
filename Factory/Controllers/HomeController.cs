a41a85ad30c7f7d69a4a633843a9669eb56941a5
Alex Bertotto <ajb5206@gmail.com>
2021-10-23 13:40:24 -0700

    add machinescontroller file with CRUD routing structure

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -4,26 +4,15 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Factory.Models;
 
 namespace Factory.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
+        [HttpGet("/")]
+        public ActionResult Index()
         {
             return View();
         }
