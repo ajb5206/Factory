824e1517695ebbe73317ed601c71e7da2fd185f3
Alex Bertotto <ajb5206@gmail.com>
2021-10-24 02:17:16 -0700

    add engineer index route and view

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+	public class EngineersController : Controller
+	{
+		private readonly FactoryContext _db;
+
+		public EngineersController(FactoryContext db)
+		{
+			_db = db;
+		}
+
+		public ActionResult Index()
+		{
+			List<Engineer> model = _db.Engineers.ToList();
+			return View(model);
+		}
+
+		
+	}
+}
